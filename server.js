7390abd3eb42f40436a45f0481490b6661ed837b
rambo2325 <rajasekhar2325@gmail.com>
2017-08-08 18:14:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,48 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content={
+     title:'article-one',
+     heading:'articleone',
+     content:` <div>
+                <p>
+                    this is the article one this a text code from me to you .
+                </p>
+                
+            </div>
+            <div>
+                <p>
+                    i do whatever it takes cause i love the adrenaline in my veins.
+                    i do whatever it takes cause i love how it feels when  i break the chains.
+                </p>
+            </div>
+}
+
+funtion createtemplate(data){
+var title=data.title;
+var content=data.content;
+var htmlTemplate=`
+    <html>
+    <head>
+        <title>
+           ${title}
+        </title>
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+    </head>
+    <body>
+        <header>
+            <a href="/">Home</a>
+        </header>
+    <hr/>
+
+    <div>
+        ${content}
+    </div>
+    </body>
+    </html>
+ `;
+ return htmlTemplate
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
