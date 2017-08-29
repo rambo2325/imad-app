a9177666d228d0993cbaeb1e94a9a09118540ea6
rambo2325 <rajasekhar2325@gmail.com>
2017-08-29 06:32:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-     res.send(createTemplate(aricleOne));
+     res.send(createTemplate(articleOne));
 });
 app.get('/article-two',function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
