6df6552b0633b73a7ef33c82348a3bc7a838aaed
rambo2325 <rajasekhar2325@gmail.com>
2017-08-29 08:26:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ button.onclick=function(){
             if(request.status===200)
             {
                 var counter=request.responseText;
-                var span=document.getElementById('count');
+                var span=document.getElementById("count");
                 span.innerhtml=count.toString();
             }
         }
