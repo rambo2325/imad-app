53f8d6826be92d89025c55841a140ad7e6642836
rambo2325 <rajasekhar2325@gmail.com>
2017-08-29 06:55:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/goku-super-saiyan-god.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'goku-super-saiyan-god.jpg'));
 });
