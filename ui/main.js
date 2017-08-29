6c66bff6d05778ac4c638614ecc0bab066d9604d
rambo2325 <rajasekhar2325@gmail.com>
2017-08-29 07:30:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="new value";
-var img=document.getElementById("goku");
+var img=document.getElementById('goku');
 img.onclick=function(){
     img.style.marginleft='100px';
 };
