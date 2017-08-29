50ae431a67163c391d4794b083e62ce419a26242
rambo2325 <rajasekhar2325@gmail.com>
2017-08-29 07:46:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,4 @@
 console.log('Loaded!');
-var element=document.getElementById("main-text");
-element.innerHTML="new value";
 var img=document.getElementById('goku');
 var marginLeft=0;
 function moveright(){
