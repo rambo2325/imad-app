642652a262c5af0443279608db57332c46ec921a
rambo2325 <rajasekhar2325@gmail.com>
2017-08-29 08:24:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,19 @@
-console.log('Loaded!');
-var img=document.getElementById('goku');
-var marginLeft=0;
-function moveright(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveright,50);
+var button=document.getElementById('counter');
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerhtml=count.toString();
+            }
+        }
+    };
+    request.open('GET','http://rajasekhar2325.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    
 };
