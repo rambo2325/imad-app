252fd6db196673ba3d7c1e542185f5c9940a06f5
rambo2325 <rajasekhar2325@gmail.com>
2017-08-29 06:23:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title:'article-one/ rambo',
+    heading:'article-one',
+    date:'sep 5 2106',
+    content:`  <p>
+                    this is the content for my first article
+                    this is the content for my first article
+                </p>`
+    
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
