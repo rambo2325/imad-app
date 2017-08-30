b2c4751a695c72151ad13bdcdb4db3e297a77f25
rambo2325 <rajasekhar2325@gmail.com>
2017-08-30 19:10:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
+var config={
+    user:'rajasekhar2325',
+    database:'rajasekhar2325',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -77,7 +84,19 @@ app.get('/counter',function(req,res)
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result)
+    {
+        if(err){
+            res.status(500).send(err,toString());
+        }
+        else{
+            res.send(JSON.strigify(result));
+        }
+         
+    });
+});
 app.get('/:articleName',function(req,res){
     articleName=req.params.articleName;
      res.send(createTemplate(articles[articleName]));
