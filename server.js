28588d7b6145106342daf6f41a65bd7ba0d9d7b0
rambo2325 <rajasekhar2325@gmail.com>
2017-08-30 19:37:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ function createTemplate(data){
                    ${heading}
                 </h3>
                 <div>
-                    ${date.toString.date}
+                    ${date.toDateString()}
                 </div>
                 <div>
                   ${content}
