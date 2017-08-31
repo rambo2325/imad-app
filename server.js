a8d1151087ad033bfea0d8d4aeb6b47f4f96be7b
rambo2325 <rajasekhar2325@gmail.com>
2017-08-31 19:29:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
-var Crypto=require('crypto');
+var crypto=require('crypto');
 
 var config={
     user:'rajasekhar2325',
